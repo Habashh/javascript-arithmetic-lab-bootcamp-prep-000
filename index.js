36352f28c3b291d01d0f3a25377c4122cfe6f8a5
zaid85j <zaid85j@gmail.com>
2020-03-25 16:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function add(a,b){
   return a+b;
 }
+<<<<<<< HEAD
 function subtract(a,b){
   return a-b;
 }
@@ -9,16 +10,29 @@ function multiply(a,b){
 }
 function divide(a,b){
   return a/b;
+=======
+function add(a,b){
+  return a+b;
+}
+function add(a,b){
+  return a+b;
+}
+function add(a,b){
+  return a+b;
+>>>>>>> 9bcdd7d3ebaaedc83b8a6a31be88cf6ca7b5cae0
 }
 function inc(n){
   return ++n;
 }
 function dec(n){
   return --n;
+<<<<<<< HEAD
 }
 function makeInt(n){
   return parseInt(n,10);
 }
 function preserveDecimal(n){
   return parseFloat(n,10);
+=======
+>>>>>>> 9bcdd7d3ebaaedc83b8a6a31be88cf6ca7b5cae0
 }
